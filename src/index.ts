a1f8e6504ce58bd0a34b55bb0dbf41c20d4c3844
liangsatan333666 <liangsatan36@gmail.com>
2026-09-17 10:45:27 +0800

    Fix Remotion entry point

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { registerRoot } from 'remotion/dom';
-import React from 'react';
-import { RemotionRoot } from './Root';
-
-registerRoot(
-  <React.StrictMode>
-    <RemotionRoot />
-  </React.StrictMode>
-);
